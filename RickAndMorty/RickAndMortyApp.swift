c9f7be2b3d0680cf262efaacc21a6a1d589ec961
Leandro Berli <leandroberli@gmail.com>
2024-10-25 13:28:30 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Leandro Berli on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
